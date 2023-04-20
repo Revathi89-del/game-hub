4ee11a2a8fa327d31e5a28b1acbf279bdefb7f11
Revathi <revathi.serene31@gmail.com>
2023-04-20 21:33:08 +0100

    Filter games by Platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,5 +14,10 @@ export interface Game {
     slug:string;
   }
 
-const useGames = (selectedGenre:Genre|null)=> useData<Game>('/games',{params:{genres:selectedGenre?.id}},[selectedGenre?.id]);
+const useGames = (selectedGenre:Genre|null,selectedPlatform:Platform|null)=> useData<Game>('/games',{params:
+  {
+    genres:selectedGenre?.id,
+    platforms:selectedPlatform?.id
+  }},
+    [selectedGenre?.id,selectedPlatform?.id]);
   export default useGames;
